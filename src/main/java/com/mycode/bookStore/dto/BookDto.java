ee6e3adc627c54588b5d1335048df3f7001b0399
srihitha-nukala <srihithanukala04@gmail.com>
2022-09-16 11:52:43 +0530

    Initial Commit

---
+++ src/main/java/com/mycode/bookStore/dto/BookDto.java
@@ -0,0 +1,23 @@
+package com.mycode.bookStore.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDto {
+    private String title;
+
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+}
